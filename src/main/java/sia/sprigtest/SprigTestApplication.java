95dfd31d657b15f1564b7fb7973cd8559d395432
Talha Javed <talhajaved024@gmail.com>
2024-01-29 20:03:26 +0500

    Project Files Added

---
+++ src/main/java/sia/sprigtest/SprigTestApplication.java
@@ -0,0 +1,14 @@
+package sia.sprigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SprigTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sia/sprigtest/SprigTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.sprigtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
